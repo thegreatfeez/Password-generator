55cfb7390203a1aa6335b3c30a572f4e48d29cc0
thegreatfeez <adamsafeez4@gmail.com>
2025-05-17 18:51:11 +0100

    newCommit
    updated

--- index.js
+++ index.js
@@ -1,42 +1,59 @@
+//Generate Two Random passwords that is 15 characters long//
+//Strecth Golas: Abilitty to set password length, Add "copy-on-click, toggle "symbols" and "numbers" on/off//
 
-const alphabetCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
-const characterNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const characterSymbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
-const passwordAmountNumber = document.getElementById("passwordAmountNumber");
-const passwordAmountRange = document.getElementById("passwordAmountRange");
-const includeNumbers = document.getElementById("numbers");
-const includeSymbols = document.getElementById("symbols");
-const passwordOneEl = document.getElementById("passwordOne-el");
-const passwordTwoEl = document.getElementById("passwordTwo-el");
+const alphabets = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M",
+  "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+  "a","b","c","d","e","f","g","h","i","j","k","l","m",
+  "n","o","p","q","r","s","t","u","v","w","x","y","z"
+];
 
-passwordAmountNumber.addEventListener("input", syncPasswordAmount);
-passwordAmountRange.addEventListener("input", syncPasswordAmount);
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-function generatePassword(){
-  const passwordAmount = passwordAmountNumber.value
-  const numbers = includeNumbers.checked
-  const symbols = includeSymbols.checked
-  let passwordOne = getPassword(passwordAmount, numbers, symbols)
-  let passwordTwo= getPassword(passwordAmount, numbers, symbols)
-  passwordOneEl.textContent = passwordOne
-  passwordTwoEl.textContent = passwordTwo
-}
+const symbols = [
+  "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",
+  "{","[","}","]",",","|",":",";","<",">",".","?","/"
+];
 
-function getPassword(passwordAmount, numbers, symbols){
-  let charCodes = alphabetCharacters
-  if (numbers) charCodes = charCodes.concat(characterNumbers)
-  if (symbols) charCodes = charCodes.concat(characterSymbols)
 
-  const passwordCharacter = []
-  for(i = 0; i < passwordAmount; i++){
-    const characters = charCodes[Math.floor(Math.random()* charCodes.length)]
-    passwordCharacter.push(characters)
+let passwordOne = document.getElementById("password1");
+let passwordTwo = document.getElementById("password2");
+let generateButton = document.getElementById("generateBtn");
+let includeNumber = document.getElementById("includeNumbers");
+let includeSymbol = document.getElementById("includeSymbols");
+
+
+
+generateButton.addEventListener("click", function () {
+  let passwordLength = document.getElementById("length").value;
+  let availableCharacters = alphabets.slice()
+  let result1 = ""
+  let result2 = ""
+
+  if (includeNumber.checked && includeSymbol.checked) {
+      availableCharacters = availableCharacters.concat(numbers,symbols);
+    } else if (includeSymbol.checked) {
+     availableCharacters = availableCharacters.concat(symbols);
+    } else if (includeNumber.checked) {
+     availableCharacters = availableCharacters.concat(numbers);
+    }
+
+
+  for (i = 0; (i < passwordLength); i++) {
+    result1 += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+    result2 += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
   }
-  return passwordCharacter.join("")
-}
+   passwordOne.value = result1
+   passwordTwo.value = result2
+});
 
-function syncPasswordAmount(e) {
-  const value = e.target.value
-  passwordAmountNumber.value = value
-  passwordAmountRange.value = value
-}
+passwordOne.addEventListener("click", function () {
+  passwordOne.select();
+  document.execCommand("copy");
+});
+
+passwordTwo.addEventListener("click", function () {
+  passwordTwo.select();
+  document.execCommand("copy");
+});
+
